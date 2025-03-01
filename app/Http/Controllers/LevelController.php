5f12b7b16a08bfc9fd8a1d3d40ad562fde46b855
Atthalaric Nero Muchtar <atthalaricnero@gmail.com>
2025-03-01 11:17:39 +0700

    praktikum6_Eloquent ORM_nero_web lanjut

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,9 +9,9 @@ class LevelController extends Controller
 {
     public function index()
     {
-        // DB::insert('insert into m_level (level_kode, level_nama, 
-        // created_at) values (?,?,?)', ['CUS', 'Pelanggan', now()]);
-        // return ('insert data baru berhasil');
+        DB::insert('insert into m_level (level_kode, level_nama, 
+        created_at) values (?,?,?)', ['CUS', 'Pelanggan', now()]);
+        return ('insert data baru berhasil');
 
         // $row = DB::update('update m_level set level_nama = ? 
         // where level_kode = ?', ['Customer', 'CUS']);
