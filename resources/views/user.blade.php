b8e99dcc1a7d4f9447027f263832f2c01ee8fe95
Atthalaric Nero Muchtar <atthalaricnero@gmail.com>
2025-03-09 13:53:05 +0700

    eloquent_orm_nero_praktikum2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,10 +5,27 @@
     <title>Data User</title>
 </head>
 
-<body></body>
+<body>
 <h1>Data User</h1>
+<a href="/user/tambah">+ Tambah User</a>
 <table border="1" cellpading="2" cellspacing="0">
     <tr>
+        <td>ID</td>
+        <td>Username</td>
+        <td>Nama</td>
+        <td>ID Level Pengguna</td>
+        <td>Aksi</td>
+    </tr>
+    @foreach ($data as $d)
+    <tr>
+        <td>{{ $d->user_id }}</td>
+        <td>{{ $d->username }}</td>
+        <td>{{ $d->nama }}</td>
+        <td>{{ $d->level_id }}</td>
+        <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+    </tr>
+    @endforeach
+    {{-- <tr>
         <th>ID</th>
         <th>Username</th>
         <th>Nama</th>
@@ -19,7 +36,7 @@
         <td>{{ $data->username }}</td>
         <td>{{ $data->nama }}</td>
         <td>{{ $data->level_id }}</td>
-    </tr>
+    </tr> --}}
     {{-- <tr>
         <th>Jumlah Pengguna</th>
     </tr>
