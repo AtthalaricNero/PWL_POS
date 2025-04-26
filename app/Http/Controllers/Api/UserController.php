e3158768af1859c70fc0519f1ffafd4685167e84
Atthalaric Nero Muchtar <atthalaricnero@gmail.com>
2025-04-26 08:00:07 +0700

    Jobsheet 10 :  Membuat RESTful API Login

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = UserModel::all();
+        return response()->json($users);
+    }
+
+}
